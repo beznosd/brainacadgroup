4c019a9c77fd684dd89f299e750e612057e3c7a4
Khavkin <hdm@ukr.net>
2016-10-04 22:34:39 +0300

    paralax home work

---
+++ paralax/js/paralax.js
@@ -0,0 +1,42 @@
+window.onscroll = function() {
+
+	var wScroll = window.pageYOffset;
+
+	// Header
+
+	var logo = document.querySelector('.logo');
+	logo.style.transform = 'translate(0px, ' + wScroll / 2 + '%)';
+
+	var backBird = document.querySelector('.back-bird');
+	backBird.style.transform = 'translate(0px, ' + wScroll / 4 + '%)';
+
+	var foreBird = document.querySelector('.fore-bird');
+	foreBird.style.transform = 'translate(0px, -' + wScroll / 30 + '%)';
+
+	// Models
+
+	var clothesPics = document.querySelector('.clothes-pics');
+	if (wScroll > clothesPics.offsetTop - 350) {
+		var figures = clothesPics.getElementsByTagName('figure');		
+		[].forEach.call(figures, function(figure, i){
+			setTimeout(function(i){
+				figure.classList.add('is-showing');
+			}, 150 * (i + 1));
+		});
+	}
+
+	/*
+	* Your code for circle with paralax
+	*/
+
+	var largeWindow = document.querySelector('.large-window'),
+		windowTint = document.querySelector('.window-tint'),
+		startPoint =largeWindow.offsetTop - document.documentElement.clientHeight;
+
+	if (wScroll > startPoint) {
+		console.log(wScroll+ ',' + startPoint + ', ' + (wScroll-startPoint) + ', ' + document.documentElement.clientHeight  );
+
+		windowTint.style.opacity = (wScroll-startPoint) / document.documentElement.clientHeight;
+	}	
+
+}
